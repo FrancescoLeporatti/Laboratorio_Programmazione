b2966f104afdfc667f7bb661539f0bc4fb8a3305
FrancescoLeporatti <francesco.leporatti@stud.unifi.it>
2021-01-26 20:21:19 +0100

    Added more descriptions to the code

--- File.cpp
+++ File.cpp
@@ -4,6 +4,8 @@ File::File(const char* filepath) : file(fopen(filepath, "r")) {
     if (!file) {
         throw std::runtime_error("Could not find file");
     } else {
+
+        // Get the size of the opened file
         fseek(file, 0, SEEK_END);
         size = static_cast<int>(ftell(file));
         std::cout << "File '" << filepath << "'(" << size <<" bytes) opened successfully." << std::endl;
